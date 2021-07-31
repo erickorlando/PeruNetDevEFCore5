a63206377304ab3b65b5ccc0b03391c1a3d1a3bd
Erick Orlando <erickorlando@outlook.com>
2021-07-30 21:59:47 -0500

    Script del SP

---
+++ PeruNetDev.DataLayer/Scripts/uspSelectContacts.sql
@@ -0,0 +1,14 @@
+﻿CREATE OR ALTER procedure [dbo].[uspSelectContacts] (@PersonId INT)
+AS
+BEGIN
+
+SELECT 
+	C.Direction AS [Address],
+	IIF(C.Principal = 1, 'YES','NO') AS Principal
+ FROM Contacts C
+WHERE C.PersonId = @PersonId
+
+END
+GO
+
+
